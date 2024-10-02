6f797aa9f1351e6664f0f3f628b090dd471b419f
João Paulo Carvalho <jjpaulo2@protonmail.com>
2024-10-02 16:27:44 -0300

    fix - handler method

--- function/app.py
+++ function/app.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from aws_lambda_powertools.utilities.parser import event_parser
+from aws_lambda_powertools.utilities.data_classes import event_source, SQSEvent
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools import Logger
 
@@ -14,13 +14,15 @@ logger = Logger(app="telegram-lambda")
 loop = asyncio.get_event_loop()
 
 
-@event_parser(model=Message)
-def handler(event: Message, context: LambdaContext):
-    message = TelegramMessage(event)
-    sender = TelegramSender(TELEGRAM_BOT_TOKENS, logger)
+@event_source(data_class=SQSEvent)
+def handler(event: SQSEvent, context: LambdaContext):
+    for record in event.records:
+        message = Message.model_validate_json(record.body)
+        message_adapter = TelegramMessage(message)
+        sender = TelegramSender(TELEGRAM_BOT_TOKENS, logger)
 
-    try:
-        loop.run_until_complete(sender.send(message))
+        try:
+            loop.run_until_complete(sender.send(message_adapter))
 
-    except Exception as exc:
-        logger.error(str(exc), exc_info=True)
+        except Exception as exc:
+            logger.error(str(exc), exc_info=True)
